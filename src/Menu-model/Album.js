a69b528371a7ae273341d5f027f1408ecd8216cc
Tutict White <70006802+tutict@users.noreply.github.com>
2023-11-07 22:42:47 +0800

    2023.11.7-22:42.42

--- src/Menu-model/Album.js
+++ src/Menu-model/Album.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-import { IonContent, IonGrid, IonRow, IonCol, IonImg, IonButton, IonModal } from '@ionic/react';
-import '../CSS/menu.css'
+import { IonContent, IonGrid, IonRow, IonCol, IonImg, IonButton, IonModal, IonPage } from '@ionic/react';
+import '../CSS/menu.css';
 import Menu from "./Menu";
-import ParticlesMenu from "./ParticleEffectMenu";
 
-const Album: React.FC = () => {
-    const [photos, setPhotos] = useState([]); // 状态不需要类型
+const Album = () => {
+    const [photos, setPhotos] = useState([]);
     const [photoToView, setPhotoToView] = useState(null);
 
-    const takePhoto = async () => {
+    const selectPhoto = async () => {
         const image = await Camera.getPhoto({
             resultType: CameraResultType.Uri,
             source: CameraSource.Photos,
@@ -21,27 +20,25 @@ const Album: React.FC = () => {
         }
     };
 
-    const viewPhoto = (photo: string) => {
+    const viewPhoto = (photo) => {
         setPhotoToView(photo);
     };
 
     return (
-        <IonContent >
+        <IonContent class="content-background-menu">
             <Menu />
-            <ParticlesMenu />
-            <IonGrid>
-                <IonRow>
-                    {photos.map((photo, index) => (
-                        <IonCol size="2" key={index}>
-                            <IonImg src={photo} onClick={() => viewPhoto(photo)} />
-                        </IonCol>
-                    ))}
-                </IonRow>
+                <IonGrid>
+                    <IonRow>
+                        {photos.map((photo, index) => (
+                            <IonCol size="2" key={index}>
+                                <IonImg src={photo} onClick={() => viewPhoto(photo)} />
+                            </IonCol>
+                        ))}
+                    </IonRow>
+                </IonGrid>
                 <div className="fixed-button-container">
-                    <IonButton fill="outline" style={{ width: '100px' }} onClick={takePhoto}>选择图片</IonButton>
+                    <IonButton fill="outline" style={{ width: '100px' }} onClick={ selectPhoto }>选择图片</IonButton>
                 </div>
-            </IonGrid>
-
             <IonModal isOpen={!!photoToView} onDidDismiss={() => setPhotoToView(null)} >
                 <IonImg src={photoToView} style={{ width: '100%', height: '100%' }} />
             </IonModal>
--- src/Menu-model/lunbotu.js
+++ src/Menu-model/lunbotu.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { IonContent, IonPage, IonButton, IonImg } from '@ionic/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+import '../CSS/menu.css';
 
-const MySwiper: React.FC = () => {
+const  Lunbotu = () => {
+    const [images, setImages] = useState([]);
+
+    const selectImage = async () => {
+        try {
+            const photo = await Camera.getPhoto({
+                resultType: CameraResultType.Uri,
+                source: CameraSource.Photos,
+                quality: 100
+            });
+
+            const newImageUrl = photo.webPath; // 从设备相册中选择的图片的路径
+
+            // 更新 images 状态，添加新的图片 URL
+            setImages(oldImages => [...oldImages, newImageUrl]);
+        } catch (error) {
+            console.error('Error accessing photos', error);
+        }
+    };
+
     return (
-        <Swiper
-            spaceBetween={50}
-            slidesPerView={1}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            {/* ... (and so on) */}
-        </Swiper>
+        <IonPage>
+            <IonContent fullscreen>
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                >
+                    {images.map((image, index) => (
+                        <SwiperSlide key={index}>
+                            <IonImg src={image} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <IonButton onClick={selectImage}>Select Image</IonButton>
+            </IonContent>
+        </IonPage>
     );
 };
 
-export default MySwiper;
+export default Lunbotu;
