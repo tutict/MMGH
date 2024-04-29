002c3c8fbeff8260d861f4a451ae9aca9c75133a
Tutict White <70006802+tutict@users.noreply.github.com>
2024-04-29 22:05:58 +0800

    2024-04-29小改动2

--- src/Menu-model/DateTimeModel.js
+++ src/Menu-model/DateTimeModel.js
@@ -1,31 +1,54 @@
 import {IonButton, IonContent, IonDatetime, IonModal} from "@ionic/react";
-import React from "react";
-const DateTimeModal = ({ isOpen, onDismiss, onConfirm, selectedDate }) => {
-    // 临时存储用户选择的时间
-    const [tempDate, setTempDate] = React.useState(selectedDate);
+import React, {useEffect, useState} from "react";
+import "../CSS/DateTimeModel.css";
 
-    // 处理时间变化
-    const handleTimeChange = (e) => {
-        // 这里假设你已经处理了时间字符串转换为 ISO 8601 字符串的逻辑
-        setTempDate(e.detail.value);
+
+const DateTimeModal = ({ isOpen, onDismiss, onConfirm }) => {
+
+    // 获取当前的日期和时间，并格式化为 ISO 字符串
+    const currentDateTime = () => {
+        const now = new Date();
+        // 北京时间比 UTC 时间快8小时
+        now.setUTCHours(now.getUTCHours() + 8);
+        return now.toISOString();
+    };
+
+    // 状态来存储用户选择的日期和时间
+    const [selectedDateTime, setSelectedDateTime] = useState(currentDateTime());
+
+    useEffect(() => {
+        // 每分钟更新北京时间
+        const intervalId = setInterval(() => {
+            setSelectedDateTime(currentDateTime());
+        }, 60000); // 60000 毫秒（1分钟）
+
+        // 清除定时器
+        return () => clearInterval(intervalId);
+    }, []);
+
+    // 处理日期时间变化
+    const handleDateTimeChange = (event) => {
+        setSelectedDateTime(event.detail.value);
     };
 
     return (
         <IonModal
             isOpen={isOpen}
-            cssClass='my-custom-class'
+            cssClass="small-modal"
             onDidDismiss={onDismiss} // 当模态框关闭时触发
             backdropDismiss={true} // 允许点击背景来关闭模态框
+            selectedDate={currentDateTime}
         >
             <IonContent>
                 <IonDatetime
                     displayFormat="HH:mm"
                     pickerFormat="HH:mm"
-                    value={tempDate}
-                    onIonChange={handleTimeChange}
+                    value={selectedDateTime}
+                    onIonChange={handleDateTimeChange}
+                    className="modal-content"
                 />
-                <IonButton onClick={onDismiss}>取消</IonButton>
-                <IonButton onClick={() => onConfirm(tempDate)}>确认</IonButton>
+                <IonButton className="modal-button-left" onClick={onDismiss}>取消</IonButton>
+                <IonButton className="modal-button-right" onClick={() => onConfirm(selectedDateTime)}>确认</IonButton>
             </IonContent>
         </IonModal>
     );
